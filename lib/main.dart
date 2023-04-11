ce553fc9dd811235e919dc1ceefd2934722a9ea5
Le Hoang-Thoai <lehoang-thoai@HoangThoai.local>
2023-04-11 23:20:15 +0700

    Lap2_Mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,111 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:lap1_mobile/personal_salary.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-));
+void main() {
+  runApp(const MyApp());
+}
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mobile Lap 20521976',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Mobile Labs 02'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final nameController = TextEditingController();
+  final grossSalaryController = TextEditingController();
+  List<PersonalSalary> listPersonalSalary = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Mobile Labs 01"),
-        elevation: 0.0,
+        title: Text(
+          widget.title,
+        ),
       ),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            color: Colors.green[200],
-            padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 50.0),
-            child: Text(""),
-          ),
-          SizedBox(
-            height: 5.0,
-          ),
-          Text("List view"),
-          SizedBox(
-            height: 5.0,
-          ),
-          Container(
-            color: Colors.red,
-            padding: EdgeInsets.symmetric(vertical: 40.0),
-          ),
-          Container(
-            color: Colors.yellow,
-            padding: EdgeInsets.symmetric(vertical: 40.0),
-          ),
-          Container(
-            color: Colors.green[200],
-            padding: EdgeInsets.symmetric(vertical: 20.0),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text("Grid view"),
-          Expanded(
-            child: Center(
-              child: GridView.count(
-                primary: false,
-                padding: const EdgeInsets.all(20),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                crossAxisCount: 2,
-                children: <Widget>[
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[100],
-                    child: const Text("He'd have you all unravel at the"),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[200],
-                    child: const Text('Heed not the rabble'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[300],
-                    child: const Text('Sound of screams but the'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[400],
-                    child: const Text('Who scream'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[500],
-                    child: const Text('Revolution is coming...'),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    color: Colors.teal[600],
-                    child: const Text('Revolution, they...'),
-                  ),
-                ],
-              ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: nameController,
+            decoration: const InputDecoration(
+              labelText: "Name",
+              border: OutlineInputBorder(),
             ),
           ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: grossSalaryController,
+            decoration: const InputDecoration(
+              labelText: "Gross Salary",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 16),
+          OutlinedButton(
+            onPressed: handleListPersonal,
+            child: const Text("Calculate"),
+          ),
+          const Divider(
+            color: Colors.black,
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: listPersonalSalary.length,
+            itemBuilder: (context, index) {
+              return personSalWidget(listPersonalSalary[index]);
+            },
+          )
         ],
       ),
     );
   }
-}
+
+  Widget personSalWidget(PersonalSalary per) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(per.fullName ?? ""),
+        const Text(": "),
+        Text("${per.netSalary}"),
+      ],
+    );
+  }
+  void addPersonalSalaryToList() {
+    // Tạo đối tượng PersonalSalary mới với thông tin từ các trường nhập liệu
+    final newPersonalSal = PersonalSalary(
+      fullName: nameController.text,
+      grossSalary: int.parse(grossSalaryController.text),
+    );
+
+    // Thêm đối tượng PersonalSalary mới vào danh sách
+    setState(() {
+      listPersonalSalary.add(newPersonalSal);
+    });
+
+    // Xóa dữ liệu đang nhập trong các trường tên và mức lương
+    nameController.clear();
+    grossSalaryController.clear();
+  }
